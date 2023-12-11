c2b4debad060c0829e4b234a604acf86dd5f79e7
Pietro Vallardi Grohe <pietrovallardigrohe@gmail.com>
2023-12-11 17:22:19 -0300

    Day 1 Complete

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::{self},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_text = fs::read_to_string("src/input.txt").unwrap();
+    let lines = input_text.lines();
+
+    let total: i32 = lines
+        .map(|line| get_first_and_last(line))
+        .collect::<Vec<i32>>()
+        .iter()
+        .sum();
+
+    println!("{total}");
+
+    Ok(())
+}
+
+fn get_first_and_last(line: &str) -> i32 {
+    println!("Line input: \"{}\"", line);
+    let current = line.chars().filter(|c| c.is_ascii_digit()).map(|c| c);
+
+    let first = current.clone().next();
+    let last = current.last();
+
+    match first {
+        Some(first) => format!("{}{}", first, last.unwrap()).parse().unwrap(),
+        None => return 0,
+    }
 }
